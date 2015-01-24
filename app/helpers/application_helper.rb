599af8fc8677d2aed5439d7fcc43fccb539cba81
kiyamakeita <kiyamakeita@gmail.com>
2015-01-25 08:30:01 +0900

    boost_befor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,11 @@
 module ApplicationHelper
 
-	#ページごと完全あタイトルを返します
 	def full_title(page_title)
 		base_title = "Ruby on Rails Tutorial Sample App"
-		if page_title.empty?
-			base_title
-		else
-			"#{base_title} | #{page_title}"
-		end
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
 	end
-
-
-
 end
